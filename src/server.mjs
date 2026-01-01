70ffaad336ae572ab566fa0f1273debb03ae9734
azeemalphahive <muhammad.azeem@alphahiveai.com>
2026-01-01 23:02:11 +0500

    fixed url issue

--- src/server.mjs
+++ src/server.mjs
@@ -22,6 +22,7 @@ import setupSocket from "./socket/index.mjs";
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:5000",
+      "https://cahsai-frontend-production.up.railway.app",
       "https://cahsai-frontend-production.up.railway.app"
     ];
     const io = new Server(httpServer, {
